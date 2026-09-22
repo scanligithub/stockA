9cc13d856e0b019d5aa25a67931da796c8a629df
scanligithub <135568972+scanligithub@users.noreply.github.com>
2026-09-22 13:09:49 +0800

    Update tdx_fetcher.go

--- scripts/tdx_fetcher.go
+++ scripts/tdx_fetcher.go
@@ -245,7 +245,7 @@ func runFetchKlinesWithLocalCSV(codesStr, gbbqPath, outPath string) {
 			defer workerCli.Close()
 
 			for tcode := range jobChan {
-				resp, err := workerCli.GetKlineDayAll(tcode)
+				 resp, err := workerCli.GetIndexDayAll(tcode)
 				if err != nil || resp == nil || len(resp.List) == 0 {
 					continue
 				}
